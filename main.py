211f148c9cc06c3d02a715ff5f8aa1e73c70c35f
Srini Vasudevan <srini@gofundraise.com>
2021-11-14 16:29:34 +1100

    update 14th nov

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from itertools import cycle
+from random import randrange
 
 canvas_width = 800
 canvas_height = 400
@@ -29,11 +31,12 @@ def verifyUser():
             if (eQuizValue == "20"):
                 user = "Welcome " + eUser
                 print(user)
+                create_eggs()
                 loginCorrect = 1
                 c.bind('<Left>', moveLeft)
                 c.bind('<Right>', moveRight)
                 c.focus_set()
-
+                
     if (loginCorrect == 0):
         print("You have entered incorrect login details")
 
@@ -59,17 +62,40 @@ c.create_oval(-80, -80, 120,120, fill="orange", width=0)
 catcher = c.create_arc(catcher_start_x, catcher_start_y, catcher_start_x2, catcher_start_y2, 
     start=200, extent=140, style="arc", outline=catcher_color, width=5)
 
+colour_list = ['blue', 'pink', 'green', 'yellow', 'red', 'light blue', 'purple']
+colour_cycle = cycle(colour_list)
+
 c.pack()
 
 def moveLeft(event):
     print("moving catcher left")
     (x1,y1,x2,y2) = c.coords(catcher)
     if (x1 > 0):
-          c.move(catcher, -20, 0)
+        c.move(catcher, -20, 0)
 
 # Homework is to finish the function moveRight so that your catcher will move right
 def moveRight(event):
     print("moving catcher right")  
+    (x1,y1,x2,y2) = c.coords(catcher)
+    if (x2 < canvas_width):
+        c.move(catcher, 20, 0)
+
+egg_width = 45
+egg_height = 55
+egg_interval = 4000
+
+#for homework, understand what we just wrote and play around
+#with the width, height and interval to see what it does to the eggs
+# as extention, create a new function called move_eggs which 
+# we will use to drop the eggs
+
+eggs = []
+def create_eggs():
+    x = randrange(20, 760)
+    y = 20
+    new_egg = c.create_oval(x,y,x+egg_width, y+egg_height,fill=next(colour_cycle),width=0)
+    eggs.append(new_egg)
+    window.after(egg_interval, create_eggs)
 
 window.mainloop()
 
